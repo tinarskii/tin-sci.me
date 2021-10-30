75c2d97f6cca020ae0c0293ee2f9f8b5dd810ef5
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-10-30 16:33:32 +0700

    Change to medium

--- pages/components/navigator.js
+++ pages/components/navigator.js
@@ -7,7 +7,7 @@ export default function Navigator() {
     <button onClick={() => location.href = "https://github.com/Tin-Tinnaphat?tab=repositories"} className={"px-4 py-2 md:m-2 flex-grow-0 w-full bg-blue-400 text-white font-bold hover:bg-blue-600 transform transition duration-200 active:scale-90 rounded-md"}>
       All Projects
     </button>
-    <button onClick={() => location.href = "https://blog.tinnaphat.xyz/"} className={"px-4 py-2 md:m-2 flex-grow-0 w-full bg-blue-300 text-white font-bold hover:bg-blue-500 transform transition duration-200 active:scale-90 rounded-md"}>
+    <button onClick={() => location.href = "https://tinnaphat.medium.com"} className={"px-4 py-2 md:m-2 flex-grow-0 w-full bg-blue-300 text-white font-bold hover:bg-blue-500 transform transition duration-200 active:scale-90 rounded-md"}>
       Blog
     </button>
     <button onClick={() => location.href = "/contact"} className={"px-4 py-2 md:m-2 flex-grow-0 w-full bg-blue-200 text-white font-bold hover:bg-blue-400 transform transition duration-200 active:scale-90 rounded-md"}>
